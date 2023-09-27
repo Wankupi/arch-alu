183671ee3a592483c39094db887cfbc0fc0bf1e3
Wankupi <2893353848@qq.com>
2023-09-27 19:11:22 +0800

    perf(adder): 组间也使用超前进位

--- addder/adder.v
+++ addder/adder.v
@@ -3,7 +3,8 @@ module CarryLookaheadAdder(
         input wire[3:0] b,
         input wire c0,
         output wire[3:0] sum,
-        output wire carry
+        output wire GG,
+        output wire PP
     );
     wire [3:0] c;
     wire[3:0] G = a & b;
@@ -14,7 +15,8 @@ module CarryLookaheadAdder(
     assign c[1] = G[0] | (c[0] & P[0]);
     assign c[2] = G[1] | (P[1] & G[0]) | (c[0] & P[0] & P[1]);
     assign c[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (c[0] & P[0] & P[1] & P[2]);
-    assign carry = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (c[0] & P[0] & P[1] & P[2] & P[3]);
+    assign GG =G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+    assign PP = P[0] & P[1] & P[2] & P[3];
 endmodule
 
 module adder16(
@@ -24,11 +26,16 @@ module adder16(
         output wire[15:0] sum,
         output wire carry
     );
-    wire[3:1] c;
-    CarryLookaheadAdder sub_adder0(a[3:0], b[3:0], c0, sum[3:0], c[1]);
-    CarryLookaheadAdder sub_adder1(a[7:4], b[7:4], c[1], sum[7:4], c[2]);
-    CarryLookaheadAdder sub_adder2(a[11:8], b[11:8], c[2], sum[11:8], c[3]);
-    CarryLookaheadAdder sub_adder3(a[15:12], b[15:12], c[3], sum[15:12], carry);
+    wire[3:0] G;
+    wire[3:0] P;
+    wire c1 = G[0] | (P[0] & c0);
+    wire c2 = G[1] | (P[1] & G[0]) | (P[1] & P[0] & c0);
+    wire c3 = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & c0);
+    assign carry = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & c0);
+    CarryLookaheadAdder sub_adder0(a[3:0], b[3:0], c0, sum[3:0], G[0], P[0]);
+    CarryLookaheadAdder sub_adder1(a[7:4], b[7:4], c1, sum[7:4], G[1], P[1]);
+    CarryLookaheadAdder sub_adder2(a[11:8], b[11:8], c2, sum[11:8], G[2], P[2]);
+    CarryLookaheadAdder sub_adder3(a[15:12], b[15:12], c3, sum[15:12], G[3], P[3]);
 endmodule
 
 module adder32(
